b73e1417d7704127b90f4317d0d77a27651edc20
Alex Whittemore <alexwhittemore@gmail.com>
2015-12-19 19:14:28 -0800

    Cleaned up the code and added detailed comments for clarity.

--- atc.ino
+++ atc.ino
@@ -24,15 +24,17 @@
 #define YELLOWLIGHT 6
 #define REDLIGHT 7
 
-// Define the voltage readings corresponding to various alcohol states. These are all determined through
-// trial and error (delicious trial and error).
-
-// From experience, the sensor should settle to sub .5. One drink IMMEDIATELY will blow high 2s
-// After 15m, I blew 1.83.
-// RIGHT after a second drink, 3.25. Quickly dropping to 2.8. 
-
-// Note that in practice, after the first party deployment of the light, these values ALL had to be adjusted.
-// Maybe it was the humidity? +10-15 degree shift in temperature? Alcohol in the air?
+/*
+ * Define the voltage readings corresponding to various alcohol states. These are all determined through
+ * trial and error (delicious trial and error).
+ * 
+ * From experience, the sensor should settle to sub .5. One drink IMMEDIATELY will blow high 2s
+ * After 15m, I blew 1.83.
+ * RIGHT after a second drink, 3.25. Quickly dropping to 2.8.
+ * 
+ * Note that in practice, after the first party deployment of the light, these values ALL had to be adjusted.
+ * Maybe it was the humidity? +10-15 degree shift in temperature? Alcohol in the air?
+ */
 
 #define SETTLEDVALUE 1.9
 #define LOWVALUE 1.8
@@ -45,7 +47,6 @@
 #define STATEWAITING 2
 #define STATEREADING 3
 #define STATERECOVERING 4
-#define STATEINDICATING 5
 
 // Declare functions, because arduino chokes and dies when these come post-call in the file, for some reason.
 void mainCode(Task* me);
@@ -60,18 +61,19 @@ void evalLights(float reading);
 //BlinkTask hartbeat(LED_PIN, 300, 200, 3);
 
 BlinkTask greenBlink(GREENLIGHT, 1000);
-BlinkTask greenIndicate(GREENLIGHT, 5000, 100, 1);
 BlinkTask yellowBlink(YELLOWLIGHT, 500);
-BlinkTask yellowIndicate(YELLOWLIGHT, 5000, 100, 1);
 BlinkTask redBlink(REDLIGHT, 500);
-BlinkTask redIndicate(REDLIGHT, 5000, 100, 1);
 
+/*
+ * taskMain does the heavy lifting once per second. PrintAlcoholValue makes sure the consol gets a constant
+ * stream of updated values for diagnostics and interactive tuning.
+ */
 Task taskMain(1000, mainCode);
 Task taskPrintAlcoholValue(500, printAlcoholValue);
 
-int runningTracker = LOW;
-
+// Start off in the "recovering" state until the sensor settles
 int state = STATERECOVERING;
+// waitCount is used in the reading state to peak-detect across a few cycles. The variables below are memory for this functionality
 int waitCount = 0;
 float currentReading;
 float lastReading;
@@ -94,31 +96,51 @@ void setup() {
   Console.begin(); 
 
 /*
+ * Enable this in order to block operation until you connect to the serial terminal.
   while (!Console){
     ; // wait for Console port to connect.
-  } */
+  }
   Console.println("You're connected to the Console!!!!");
+*/
+
   //Start the timers
   SoftTimer.add(&taskMain);
   SoftTimer.add(&taskPrintAlcoholValue);
 }
 
 void mainCode(Task* me) {
+  // Run the peak detect code. This gets reset in the state machine if it's not pertinent.
   waitCount = waitCount + 1;
   lastReading = currentReading;
   currentReading = getAlcoholVoltage();
+  // Detect a new peak.
   if (currentReading > maxReading) {
     maxReading = currentReading;
   }
+
+  // State machine.
   switch (state) {
     case STATERECOVERING:
     {
+      /*
+       * In the "recovering" state, we're waiting for the sensor resistance to drop to some known low value, indicating a 
+       * settled reading. Low resistance corresponds to low voltage on the analog reading. 
+       */
+       
+      /*
+       * I'm not quite sure why this manual pin write is necessary - the blinker should run if we're to remain in this state,
+       * and shut off if we're to transition out. But for some reason, it gets hung up. This fixes it, so I never debugged
+       * exactly why. It's tough to do without a proper debugger.
+       */
       digitalWrite(REDLIGHT, LOW);
+      // Wait count is not pertinent here, so make sure it's reset.
       waitCount = 0;
       Console.println("Recovering");
       if (currentReading > SETTLEDVALUE) {
+        // we should stay in this state because the value is not settled to below our threshold yet.
         redBlink.start();
       } else {
+        // Stop the red blinking light and transition to the "waiting" state.
         redBlink.stop();
         state = STATEWAITING;
       }
@@ -126,6 +148,12 @@ void mainCode(Task* me) {
     break;
     case STATEWAITING:
     {
+      /*
+       * In the waiting state, we're waiting to detect a movement on the sensor value, indicating someone breathing booze-breaht.
+       * Once we see this shift in value, we know to flash the yellow indicator until we've detected a good peak.
+       */
+
+      // Reset the wait count and peak detection in prep for the reading state.
       waitCount = 0;
       maxReading = 0;
       Console.println("Waiting");
@@ -137,6 +165,9 @@ void mainCode(Task* me) {
     break;
     case STATEREADING:
     {
+      /*
+       * In the reading state, we're trying to peak-detect. Flash a yellow indicator to the user to keep breathing, until we do.
+       */
       yellowBlink.start();
       Console.println("Reading");
       // Reset the waitcount if we're still moving.
@@ -153,18 +184,6 @@ void mainCode(Task* me) {
       }
     }
     break;
-    /*
-    case STATEINDICATING:
-    {
-      if (waitCount > 5) {
-        greenIndicate.stop();
-        yellowIndicate.stop();
-        redIndicate.stop();
-        state = STATERECOVERING;
-      }
-    }
-    break;
-    */
   }
 }
 
@@ -173,6 +192,7 @@ void printAlcoholValue(Task* me) {
 }
 
 float getAlcoholVoltage() {
+  // Calculate voltage from binary word.
   return analogRead(SENSOR) * (5.0 / 1023.0);
 }
 
@@ -180,19 +200,17 @@ void evalLights(float reading) {
   Console.println("Evaluating:");
   Console.println(reading);
   Console.println("reading above");
-  
+
+  // Depending on the read value, compare it to our thresholds and indicate for 5s the proper drunkenness level.
   if (reading <= LOWVALUE) {
-    //greenIndicate.start();
     digitalWrite(GREENLIGHT, HIGH);
     delay(5000);
     digitalWrite(GREENLIGHT, LOW);
   } else if (reading <= MEDVALUE) {
-    //yellowIndicate.start();
     digitalWrite(YELLOWLIGHT, HIGH);
     delay(5000);
     digitalWrite(YELLOWLIGHT, LOW);
   } else if (reading > MEDVALUE) {
-    //redIndicate.start();
     digitalWrite(REDLIGHT, HIGH);
     delay(5000);
     digitalWrite(REDLIGHT, LOW);
